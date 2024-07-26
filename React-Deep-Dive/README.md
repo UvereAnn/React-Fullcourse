77cb3c0dbbc7924512f37d694d4b3e60e638b439
Uvere Ann <uvereann@gmail.com>
2024-07-27 00:41:10 +0100

    Created the Tic-Tac-Toc Game in React-Deep-Dive folder

---
+++ React-Deep-Dive/README.md
@@ -0,0 +1,16 @@
+This folder contains the Tic-Tac-Toe game.
+
+React concepts used are:
+
+- fragments
+- forwarding props to wrapped elements
+- two-way binding
+- updating object state immutably
+- lifting state up
+- avoind intersecting states
+- deriving state from props
+- sharing states across components
+- disabling buttons conditionally
+- image storgae
+- nested list
+- updating a state based on old state correctly
---
+++ React-Deep-Dive/src/components/Player.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const Player = ({ initialName, symbol, isActive }) => {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    //btnCaption = "Save";
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      {/*<button onClick={handleEditClick}>{btnCaption}</button>*/}
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
+
+export default Player;
