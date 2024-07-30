446c6d6d84882d76efc02c98fb80830c1bb58b80
Uvere Ann <uvereann@gmail.com>
2024-07-31 00:41:37 +0100

    Added UserInput component

--- Investment-Calculator/src/App.jsx
+++ Investment-Calculator/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
   return (
     <>
       <Header />
+      <UserInput />
     </>
   );
 }
