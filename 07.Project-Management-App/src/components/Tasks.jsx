f13f4d6291cb6042fd0e0dd6c428faddadecb6c3
Uvere Ann <uvereann@gmail.com>
2024-08-17 18:51:45 +0100

    worked on clearing/deleting tasks

--- 07.Project-Management-App/src/components/Tasks.jsx
+++ 07.Project-Management-App/src/components/Tasks.jsx
@@ -20,7 +20,10 @@ const Tasks = ({ tasks, onAdd, onDelete }) => {
             my-4"
             >
               <span>{task.text}</span>
-              <button className="text-stone-700 hover:text-red-500">
+              <button
+                className="text-stone-700 hover:text-red-500"
+                onClick={() => onDelete(task.id)}
+              >
                 Clear
               </button>
             </li>
