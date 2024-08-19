df5e08a469498b637c1ce23e43f123a83e38a67d
Uvere Ann <uvereann@gmail.com>
2024-08-19 19:50:45 +0100

    linked the context to state

--- 08.Advanced-State-Management/src/App.jsx
+++ 08.Advanced-State-Management/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import Header from "./components/Header.jsx";
-import Shop from "./components/Shop.jsx";
-import Product from "./components/Product.jsx";
-import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import { CartContext } from "./store/shopping-cart-context.jsx";
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import Product from './components/Product.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { CartContext } from './store/shopping-cart-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -67,8 +67,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart
+  };
+
   return (
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
